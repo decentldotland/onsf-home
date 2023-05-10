6042120df758c4855dd962c8042bd88a50d8f261
xylophonez <xylophonezygote@gmail.com>
2023-05-10 06:44:31 +0100

    fix: mobile: text-overspill in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,9 +13,9 @@ function Hero({ tracksRef }) {
     <section className="bg-black text-white relative">
       <img src={Logo} alt="Logo" width={50} className="absolute top-2 left-2" />
       <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-indigo-400/20 via-black/0 via-black/0 via-black/0 to-black/0">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto max-w-6xl text-center">
-            <h1 className="font-hero bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 bg-clip-text text-7xl font-extrabold text-transparent sm:text-7xl">
+            <h1 className="font-hero bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 bg-clip-text text-4xl font-extrabold text-transparent sm:text-7xl">
               Open Name Service Framework
               <span className="sm:block">  </span>
             </h1>
